002b3bab82935570342971c9646610473e78c433
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2010-08-18 01:13:22 +0000

    Slightly modified the calling style to erosprotect, added script and doc'd.

--- tools/eros_python_tools/src/eros_python_tools/protect.py
+++ tools/eros_python_tools/src/eros_python_tools/protect.py
@@ -13,6 +13,7 @@ import subprocess
 from eros_python_tools.core import ros_version
 
 def min_packages():
+    ''' Return the minimal set of packages needed for a cross compiled ros runtime. '''
     version = ros_version()
     if ( version == "boxturtle" ):
         packages = ["rospack", "genmsg_cpp", "roslang", "xmlrpcpp", "pycrypto", "paramiko", "roslib", "rospy", "rosconsole", "roscpp", "rosout", "roslaunch", "rostest"]
@@ -25,11 +26,13 @@ def min_packages():
 
 # Often we need to kickstart the process manually (without rospack, we can't do rosmake!)
 def make_rospack():
+    ''' Cleans and recompiles rospack. '''
     rospack_dir = os.getenv("ROS_ROOT")+"/tools/rospack"
     unused_retcode = subprocess.call(["make", "clean"],cwd=rospack_dir)
     unused_retcode = subprocess.call(["make"],cwd=rospack_dir)
     
 def protect_ros_core():
+    ''' Cleans, then recompiles the ros core stack, finally adds ROS_NOBUILD to all packages.'''
     unprotect_ros_core()
     make_rospack()
     ros_package_path = os.getenv('ROS_PACKAGE_PATH')
@@ -39,6 +42,7 @@ def protect_ros_core():
     os.environ['ROS_PACKAGE_PATH'] = ros_package_path
 
 def unprotect_ros_core():
+    ''' Removes any ROS_NOBUILD flags from all ros core stack packages.'''
     ros_package_path = os.getenv('ROS_PACKAGE_PATH')
     os.environ['ROS_PACKAGE_PATH'] = os.getenv('ROS_ROOT')
     p = subprocess.Popen(["rosmake", "-u", "-a"])
@@ -46,39 +50,39 @@ def unprotect_ros_core():
     os.environ['ROS_PACKAGE_PATH'] = ros_package_path
 
 def protect_min():
+    ''' Cleans, then recompiles a minimal set of ros core packages, finally adds ROS_NOBUILD to all packages.'''
     unprotect_ros_core()
     make_rospack()
     p = subprocess.Popen(["rosmake", "-i", "--pre-clean", "--rosdep-install", "--rosdep-yes"] + min_packages())
     p.wait()
 
 def protect_main():
+    ''' Main entry point for creating an erosprotect script.'''
     from optparse import OptionParser
-    usage = "Usage: %prog [options]"
+    usage = "Usage: %prog [options] <core|minimal>"
     parser = OptionParser(usage=usage)
-    parser.add_option("-r","--ros", action="store_true", dest="ros", default=False, help="Build/protect the entire ros core stack.")
-    parser.add_option("-m","--minimal", action="store_true", dest="minimal", default=False, help="Only build/protect the minimal set of packages required for cross-compiling.")
     parser.add_option("-u","--unprotect", action="store_true", dest="unprotect", default=False, help="Unprotect the ros core stack.")
     options, args = parser.parse_args()
     
     ###################
     # Abort Checks
     ###################
-    if args:
-        parser.error("Only options are permitted, run with --help to see options.")
-
+    if not args:
+        parser.error("You must specify a target. Valid targets include [core|minimal].")
+    
     ###################
     # Action
     ###################
     if ( options.unprotect ):
         unprotect_ros_core()
-    if options.ros:
-        protect_ros_core()
         sys.exit(0);
-    elif options.minimal:
-        protect_min()
-        sys.exit(0);
-    else:
-        parser.error("No options supplied - run with --help to see options.")
+    if args:
+        target = args[0]
+        if ( target == "core" ):
+            protect_ros_core()
+        elif ( target == "minimal" ):
+            protect_min()
+        parser.error("Only options are permitted, run with --help to see options.")
         
 if __name__ == "__main__":
     protect_main()
