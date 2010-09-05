65c594e19d9edac5aaa6d88e3445d7b87a3c4848
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2010-09-05 14:48:32 +0000

    eros_toolchains : revised the format for toolchains.

--- tools/eros_toolchains/library/crossdev/i686-pc-linux-gnu.cmake
+++ tools/eros_toolchains/library/crossdev/i686-pc-linux-gnu.cmake
@@ -4,28 +4,19 @@
 # Target Root : /usr/i686-pc-linux-gnu
 ###############################################################################
 
-# This one is important
-set(CMAKE_SYSTEM_NAME Linux)
-
-# Some useful variables that uniquely define this toolchain module
-set(TOOLCHAIN_GROUP "crossdev")
+# Some useful custom variables that uniquely define this toolchain module
+set(TOOLCHAIN_FAMILY "crossdev")
 set(TOOLCHAIN_TUPLE "i686-pc-linux-gnu" CACHE STRING "Toolchain signature identifying cpu-vendor-platform-clibrary.")
-set(TARGET_ROOT "/usr/${TOOLCHAIN_TUPLE}" CACHE STRING "Root of the target development environment (libraries, headers etc).")
+set(TOOLCHAIN_SYSROOT "/usr/${TOOLCHAIN_TUPLE}" CACHE STRING "Root of the target development environment (libraries, headers etc).")
 
-# Specify the cross compiler - make sure it is in your PATH
-set(CMAKE_C_COMPILER   ${TOOLCHAIN_TUPLE}-gcc)
+# Now the cmake variables
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_TUPLE}-gcc) # Make sure these are in your PATH
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_TUPLE}-g++)
-
-# Configure search directory for headers and libs (for the find function).
-set(CMAKE_FIND_ROOT_PATH ${TARGET_ROOT} CACHE STRING "Cmake search variable for finding libraries/headers.")
-MARK_AS_ADVANCED(CMAKE_FIND_ROOT_PATH)
-
-# Search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
-# For libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_SYSROOT} CACHE STRING "Cmake search variable for finding libraries/headers.")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) # Don't search for programs in sysroot
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)  # Headers and libs from sysroot only
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Advanced variables - hide from the front page of cache
-MARK_AS_ADVANCED(CMAKE_GENERATOR CMAKE_TOOLCHAIN_FILE)
+# Hide from cache's front page
+MARK_AS_ADVANCED(CMAKE_GENERATOR CMAKE_FIND_ROOT_PATH CMAKE_TOOLCHAIN_FILE)
