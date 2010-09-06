00446426e19f028838273a3ddd89b90d0752bb87
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2010-09-06 14:02:21 +0000

    eros_toolchains : added a few toolchain modules and updated the doxygen.

---
+++ tools/eros_toolchains/library/crossdev/i686-pc-mingw32.cmake
@@ -0,0 +1,22 @@
+###############################################################################
+# Family : crossdev
+# Tuple : i686-pc-mingw32
+# Sysroot : /usr/i686-pc-mingw32
+###############################################################################
+
+# Some useful custom variables that uniquely define this toolchain module
+set(TOOLCHAIN_FAMILY "crossdev")
+set(TOOLCHAIN_TUPLE "i686-pc-mingw32" CACHE STRING "Toolchain signature identifying cpu-vendor-platform-clibrary.")
+set(TOOLCHAIN_SYSROOT "/usr/${TOOLCHAIN_TUPLE}" CACHE STRING "Root of the target development environment (libraries, headers etc).")
+
+# Now the cmake variables
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_TUPLE}-gcc) # Make sure these are in your PATH
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_TUPLE}-g++)
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_SYSROOT} CACHE STRING "Cmake search variable for finding libraries/headers.")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) # Don't search for programs in sysroot
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)  # Headers and libs from sysroot only
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Hide from cache's front page
+MARK_AS_ADVANCED(CMAKE_GENERATOR CMAKE_FIND_ROOT_PATH CMAKE_TOOLCHAIN_FILE)
