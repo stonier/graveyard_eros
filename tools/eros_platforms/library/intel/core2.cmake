35001604a3e61ab4b748079faf40ae7f354014de
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2010-09-08 13:36:23 +0000

    eros_platforms : added the default platform.

--- tools/eros_platforms/library/intel/core2.cmake
+++ tools/eros_platforms/library/intel/core2.cmake
@@ -1,13 +1,14 @@
 ###############################################################################
-# Platform : core2
 # Family : intel
+# Platform : core2
 ###############################################################################
 
 # Some useful custom variables that uniquely define this platform module
-set(PLATFORM_NAME "core2" CACHE STRING "Platform name, usually referring to the cpu architecture.")
 set(PLATFORM_FAMILY "intel" CACHE STRING "Platform family, usually referring to intel/arm etc.")
+set(PLATFORM_NAME "core2" CACHE STRING "Platform name, usually referring to the cpu architecture.")
 
-# Compile flags
+# Flags
 rosbuild_check_for_sse() # this sets the SSE_FLAGS variable
-set(PLATFORM_COMPILE_FLAGS "-march=core2 -pipe -mmmx ${SSE_FLAGS}" CACHE STRING "Compile flags specific to this cpu.")
+set(PLATFORM_COMPILE_FLAGS "-march=core2 -pipe -mmmx ${SSE_FLAGS}" CACHE STRING "Compile flags specific to this platform.")
+set(PLATFORM_LINK_FLAGS "" CACHE STRING "Link flags specific to this platform.")
 
