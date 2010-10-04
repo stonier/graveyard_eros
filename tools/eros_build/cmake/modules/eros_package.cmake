b1ef9b0ecfcb5c313892c8ceee2d70a309e890dc
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2010-10-04 00:48:06 +0000

    eros_build : some fixes after road testing the ecl_apr build.

--- tools/eros_build/cmake/modules/eros_package.cmake
+++ tools/eros_build/cmake/modules/eros_package.cmake
@@ -17,14 +17,14 @@ endmacro()
 #
 # include($ENV{ROS_ROOT}/core/rosbuild/rosbuild.cmake)
 # rosbuild_init()
-# rosbuild_include(ecl_build eclbuild.cmake)
+# rosbuild_include(eros_build eclbuild.cmake)
 #
 
-# This is the init used for building 3rd party packages in ecl_cross.
+# This is the init used for building 3rd party packages in eros_cross.
 macro(eros_3rd_party_init)
     rosbuild_find_ros_package(eros_build)
     set(CMAKE_MODULE_PATH ${eros_build_PACKAGE_PATH}/cmake/modules)
-    include(ecl_build_utilities)
-    ecl_version()
-    ecl_add_uninstall_target()
+    include(eros_build_utilities)
+    eros_version()
+    eros_add_uninstall_target()
 endmacro()
