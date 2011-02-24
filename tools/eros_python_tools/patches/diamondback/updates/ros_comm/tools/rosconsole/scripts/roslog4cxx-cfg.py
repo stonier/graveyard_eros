090c393a4dbfd70b5bf6980c7656c5aa0a0383a8
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2011-02-24 14:53:55 +0000

    eros_python_tools : mingw rosconsole updated to handle static compiles.

--- tools/eros_python_tools/patches/diamondback/updates/ros_comm/tools/rosconsole/scripts/roslog4cxx-cfg.py
+++ tools/eros_python_tools/patches/diamondback/updates/ros_comm/tools/rosconsole/scripts/roslog4cxx-cfg.py
@@ -35,11 +35,11 @@ import sys
 import os
 #import string
 #from glob import glob
-#import subprocess
+import subprocess
 #import platform
-#from optparse import OptionParser
+from optparse import OptionParser
 #from cStringIO import StringIO
-#import tempfile
+import tempfile
 import roslib
 
 def print_usage_and_exit():
@@ -52,70 +52,66 @@ def print_usage_and_exit():
 
 def check_for_toolchain_tuple():
     if ( not os.path.exists(os.path.join(roslib.rosenv.get_ros_root(),"rostoolchain.cmake"))):
-        return ''
+        return None
     cmake_script = tempfile.NamedTemporaryFile(mode='w+t')
     cmake_script.write("include($ENV{ROS_ROOT}/rostoolchain.cmake)\n")
     cmake_script.write("message(${TOOLCHAIN_TUPLE})\n")
     cmake_script.seek(0)
     result = subprocess.Popen(['cmake', '-P', cmake_script.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    # print "Result: " + result[0].rstrip()
-    # print "Error: " + result[1].rstrip()
     cmake_script.close()
     # no idea why cmake makes this come out on stderr, but its fine
     return result[1].rstrip()
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def main():
     if (len(sys.argv) < 2):
       print_usage_and_exit()
     
     parser = OptionParser()
-    parser.add_option("-l", "--libs", dest="libs", type="string", help="")
+    parser.add_option("-l", "--libs", dest="libs", action="store_true", default=False, help="")
     parser.add_option("-c", "--cflags", dest="cflags", action="store_true", default=False, help="")
     # parser.add_option("-i", "--include_dirs", dest="include_dirs", action="store_true", default=False, help="")
     # parser.add_option("-d", "--lib_dirs", dest="lib_dirs", action="store_true", help="")
     # parser.add_option("-f", "--lflags", dest="lflags", type="string", help="")
     
     (options, args) = parser.parse_args()
-    
-              
-    ver = find_boost(search_paths(options.sysroot))
-    if (ver is None):
-        raise BoostError("Cannot find boost in any of %s"%search_paths(options.sysroot))
-        sys.exit(0)
-    
-    if (options.version):
-        check_one_option(options, 'version')
-        print '%s.%s.%s root=%s include_dir=%s'%(ver.major, ver.minor, ver.patch, ver.root, ver.include_dir)
-        return
-    
-    if (ver.major < 1 or (ver.major == 1 and ver.minor < 37)):
-        raise BoostError('Boost version %s.%s.%s does not meet the minimum requirements of boost 1.37.0'%(ver.major, ver.minor, ver.patch))
 
-    sysroot = check_for_toolchain_sysroot()
-    
-    output = ""
-    if (options.root):
-        check_one_option(options, 'root')
-        output = ver.root
-    elif (options.libs):
-        check_one_option(options, 'libs')
-        output = libs(ver, options.libs.split(','))
-    elif (options.include_dirs):
-        check_one_option(options, 'include_dirs')
-        output = include_dirs(ver)
-    elif (options.lib_dirs):
-        check_one_option(options, 'lib_dirs')
-        output = lib_dirs(ver)
-    elif (options.cflags):
-        check_one_option(options, 'cflags')
-        output = cflags(ver)
-    elif (options.lflags):
-        check_one_option(options, 'lflags')
-        output = lflags(ver, options.lflags.split(','))
+    tuple = check_for_toolchain_tuple()
+    if ( tuple == None ):
+        pkg_cfg_filename = "pkg-config"
     else:
-        print_usage_and_exit()
-    
-    print output.strip()
+        pkg_cfg_filename = tuple+"-pkg-config"
+    pkg_cfg_pathname = which(pkg_cfg_filename)
+    # Can't find <tuple>-pkg-config, just set -llog4cxx and hope for the best.
+    # Note, we might want to update this when cross-compiling to use the host 
+    # with some env. variables instead of the cross pkg-config 
+    if ( pkg_cfg_pathname == None ):
+        print "-llog4cxx"
+        return 0
 
+    # Execute        
+    if(options.libs):
+       libs = subprocess.Popen([pkg_cfg_pathname, '--libs', 'liblog4cxx'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+       print libs.rstrip()  #get rid of the newline pkg-config adds
+    elif (options.cflags):
+       cflags = subprocess.Popen([pkg_cfg_pathname, '--cflags', 'liblog4cxx'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+       print cflags.rstrip() #get rid of the newline pkg-config adds
+    return 0
+    
 if __name__ == "__main__":
     main()
