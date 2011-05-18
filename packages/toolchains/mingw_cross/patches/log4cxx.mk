acf0b2155cacb69e54e4bbf746c04f939480265b
snorri <snorri@e06aebad-a61f-4bce-bd80-11f738b071f2>
2011-05-18 01:38:33 +0000

    mingw_cross : move to more stable log4cxx url's.

--- packages/toolchains/mingw_cross/patches/log4cxx.mk
+++ packages/toolchains/mingw_cross/patches/log4cxx.mk
@@ -22,8 +22,8 @@ $(PKG)_CHECKSUM := d79c053e8ac90f66c5e873b712bb359fd42b648d
 $(PKG)_SUBDIR   := apache-log4cxx-$($(PKG)_VERSION)
 $(PKG)_FILE     := apache-log4cxx-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://logging.apache.org/log4cxx/index.html
-$(PKG)_URL      := http://apache.naggo.co.kr//logging/log4cxx/0.10.0/$($(PKG)_FILE)
-$(PKG)_URL_2    := http://apache.mirror.cdnetworks.com//logging/log4cxx/0.10.0/$($(PKG)_FILE)
+$(PKG)_URL    := http://apache.mirror.cdnetworks.com/logging/log4cxx/0.10.0/$($(PKG)_FILE)
+$(PKG)_URL_2      := http://apache.naggo.co.kr//logging/log4cxx/0.10.0/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc apr-util
 
 #define $(PKG)_UPDATE
